97294de4b4086bf5a8cbb65d258d7dd51aada872
strosi <stoeva.rositsa@gmail.com>
2022-01-14 10:23:07 +0200

    Layout adjustments

---
+++ faq-accordion-card-main/js/box-anim.js
@@ -0,0 +1,36 @@
+/**
+ * Short animation that moves aside the box image 
+ * and is triggered when the mouse hover a question label.
+ * 
+ * Information for matchMedia from 
+ * https://css-tricks.com/working-with-javascript-media-queries/
+ */
+
+let box = document.querySelector(".box-img");
+let questHeader = document.querySelectorAll(".faq__question>h2");
+
+let mediaQueryMedium = window.matchMedia('(min-width: 800px)');
+
+// Use media query check to prevent adding listeners in mobile view
+// because then the box image is removed from the view.
+
+function handleWidthChange(e) {
+    if (e.matches) {
+        console.log('Listener is added!');
+    
+        questHeader.forEach(header => {
+            header.addEventListener("mouseover", () => {
+                box.classList.add("move");
+            });
+            header.addEventListener("mouseout", () => {
+                box.classList.remove("move");
+        
+            })
+        });
+    }
+}
+
+mediaQueryMedium.addListener(handleWidthChange);
+
+// Initial check
+handleWidthChange(mediaQueryMedium);
