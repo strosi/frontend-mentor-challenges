657130163c310ebb3bc30d5472cbc6cd8debc27b
strosi <stoeva.rositsa@gmail.com>
2022-01-19 17:04:48 +0200

    Tooltip adjustments

--- article-preview-component-master/js/index.js
+++ article-preview-component-master/js/index.js
@@ -1,3 +1,23 @@
+// 650 px is the min-width of media query.
+const mqMaxWidth = 650;
+
+const isTooltipOpen = () => {
+    const tooltip = document.querySelector("#sharelinks");
+    const isOpen = tooltip.classList.contains("open");
+
+    return isOpen;
+}
+
+const showTooltip = (isOpen) => {
+    const tooltip = document.querySelector("#sharelinks");
+
+    if(!isOpen) {
+        tooltip.classList.add("open");
+    } else {
+        tooltip.classList.remove("open");
+    }
+}
+
 const changeBtnStyle = (isOpen) => {
     const btn = document.querySelector("#tooltipbtn");
 
@@ -8,26 +28,52 @@ const changeBtnStyle = (isOpen) => {
     }
 }
 
-const toggleTooltip = () => {
-    // const btn = document.querySelector("#tooltipbtn");
+const moveFooterBox = (isOpen) => {
     const footerBox = document.querySelector(".article-content__footer");
-    const authorInfo = document.querySelector(".article-content__author-info");
-    const tooltip = document.querySelector("#sharelinks");
-    const isOpen = tooltip.classList.contains("open");
-
-    console.log(isOpen);
 
     if(!isOpen) {
-        tooltip.classList.add("open");
-        // btn.classList.add("dark-btn");
-        changeBtnStyle(isOpen);
         footerBox.classList.add("move-footer");
+    } else {
+        footerBox.classList.remove("move-footer");
+    }
+}
+
+const hideAuthor = (isOpen) => {
+    const authorInfo = document.querySelector(".article-content__author-info");
+
+    if(!isOpen) {
         authorInfo.classList.add("hidden");
     } else {
-        tooltip.classList.remove("open");
-        // btn.classList.remove("dark-btn");
-        changeBtnStyle(isOpen);
-        footerBox.classList.remove("move-footer");
         authorInfo.classList.remove("hidden");
+    }
+}
+
+const toggleTooltip = () => {
+    const isOpen = isTooltipOpen();
+
+    showTooltip(isOpen);
+    changeBtnStyle(isOpen);
+
+    // Only in mobile view the author would be hidden and
+    // the footer's padding-bottom would be reduced.
+    if(window.innerWidth < mqMaxWidth) {
+        moveFooterBox(isOpen);
+        hideAuthor(isOpen);
+    }
+}
+
+window.onresize = () => {
+    const isOpen = isTooltipOpen();
+
+    // While resizing only on window width<650px the author would be hidden and
+    // the footer's padding-bottom would be reduced.
+    if(isTooltipOpen()) {
+        if(window.innerWidth > mqMaxWidth) {
+            moveFooterBox(isOpen);
+            hideAuthor(isOpen);
+        } else {
+            moveFooterBox(!isOpen);
+            hideAuthor(!isOpen);
+        }
     }
 }
