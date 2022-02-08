9dfb49b63df2813808235f8f7f9daf53be6c48a2
strosi <stoeva.rositsa@gmail.com>
2022-02-08 13:30:50 +0200

    Add options menu button JS, CSS

--- junior/time-tracking-dashboard-main/js/index.js
+++ junior/time-tracking-dashboard-main/js/index.js
@@ -22,47 +22,75 @@ window.onload = () => {
 /*  ---------------------------------------------------
     Create the initial HTML structure 
     for the report statistic entity.
-*/
+ */
 const createStatistic = (title, timeframe, timefrName) => {
-    // Report statistic header
+    // Report statistic header -----------------------|
+    // - Title...
     const statTitle = document.createElement('h2');
     const titleText = document.createTextNode(title);
     statTitle.appendChild(titleText);
     statTitle.classList.add('stat-content__title');
 
+    // - Options menu button...
+    // -- Button
+    const togBtn = document.createElement('button');
+    togBtn.classList.add('stat-content__tog-btn');
+    togBtn.setAttribute('aria-haspopup', true);
+    togBtn.setAttribute('aria-expanded', false);
+    // -- Button elements
+    for(let i=0; i<4; i++) {
+        const btnEl = document.createElement('span');
+        btnEl.classList.add('stat-content__btn-el');
+        togBtn.append(btnEl);
+    }
+    // const btnEl01 = document.createElement('span');
+    // btnEl01.classList.add('stat-content__btn-el');
+    // const btnEl02 = document.createElement('span');
+    // btnEl02.classList.add('stat-content__btn-el');
+    // const btnEl03 = document.createElement('span');
+    // btnEl03.classList.add('stat-content__btn-el');
+    // const btnEl04 = document.createElement('span');
+    // btnEl04.classList.add('stat-content__btn-el');
+
+    // togBtn.append(btnEl01, btnEl02, btnEl03, btnEl04);
+
     const statMenu = document.createElement('div');
     statMenu.classList.add('stat-content__menu');
+    statMenu.appendChild(togBtn);
 
+    // - Header container...
     const statHeadContainer = document.createElement('div');
     statHeadContainer.classList.add('stat-content__header');
     statHeadContainer.appendChild(statTitle);
     statHeadContainer.appendChild(statMenu);
 
-    // Report statistic data
+    // Report statistic data -------------------------|
+    // - Current...
     const statCurr = document.createElement('p');
     const currentStat = document.createTextNode(timeframe.current + 'hrs');
     statCurr.appendChild(currentStat);
     statCurr.classList.add('stat-content__current');
 
+    // - Previous...
     const statPrevLabel = getPrevStatLabel(timefrName);
     const statPrev = document.createElement('p');
     const previousStat = document.createTextNode(statPrevLabel + timeframe.previous + 'hrs')
     statPrev.appendChild(previousStat);
     statPrev.classList.add('stat-content__prev');
 
+    // - Data container
     const statDataContainer = document.createElement('div');
     statDataContainer.classList.add('stat-content__data');
     statDataContainer.appendChild(statCurr);
     statDataContainer.appendChild(statPrev);
 
-    // Statistic container
+    // Statistic container ---------------------------|
     const statContentContainer = document.createElement('div');
     statContentContainer.classList.add('stat-content');
     statContentContainer.id = title.toLowerCase().replace(/\s/g, "");
     statContentContainer.appendChild(statHeadContainer);
     statContentContainer.appendChild(statDataContainer);
 
-    // statistic_work-theme
     const themeClassName = 'statistic_' + statContentContainer.id + '-theme'
     const container = document.createElement('div');
     container.classList.add('statistic', themeClassName);
@@ -73,7 +101,7 @@ const createStatistic = (title, timeframe, timefrName) => {
 
 /*  ---------------------------------------------------
     Handle checked event of the time frame radio button.
-*/
+ */
 const handleClick = (e, data) => {
     const currentTimefr = e.target.id;
     changeTimeFrame(data, currentTimefr);
@@ -82,7 +110,7 @@ const handleClick = (e, data) => {
 /*  ---------------------------------------------------
     Refresh statistical data according to the time frame option
     that was checked.
-*/
+ */
 const changeTimeFrame = (data, btnId) => {
     data.forEach(d => {
         changeStatistic(d.title, getCurrentTimefr(d, btnId), btnId);
@@ -91,7 +119,7 @@ const changeTimeFrame = (data, btnId) => {
 
 /*  ---------------------------------------------------
     Change statistical data of an entity.
-*/
+ */
 const changeStatistic = (title, timeframe, timefrName) => {
     const id = title.toLowerCase().replace(/\s/g, "");
     const statContent = document.getElementById(id);
@@ -101,6 +129,9 @@ const changeStatistic = (title, timeframe, timefrName) => {
     statContent.childNodes[1].lastChild.innerHTML = statPrevLabel + timeframe.previous + 'hrs';
 }
 
+/*  ---------------------------------------------------
+    Helpers
+ */
 const getCurrentTimefr = (data, checkedTimefr) => {
     let currentTimefr = {};
 
